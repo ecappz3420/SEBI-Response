c0650ee11da7e3ae13847027f0ea9c17dc5a8112
ecappz3420 <info@ecappz.in>
2024-11-04 00:51:13 +0530

    Add: Loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -15,34 +15,35 @@ const App = () => {
   const [functionValue, setFunctionValue] = useState("");
   const [objective, setObjective] = useState("");
   const [standardSet, setStandardSet] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetch = async () => {
+      setLoading(true);
       await ZOHO.CREATOR.init();
       const queryParams = await ZOHO.CREATOR.UTIL.getQueryParams();
       const getRecords = await fetchRecords("All_Questionnaire_Line_Items", `Questionnaire_Form == ${queryParams.QID}`);
-      
-      // Extract unique values for the dropdowns
       const getGoals = [...new Set(getRecords.map(rec => rec.CR_Goal.display_value))];
       const getFunctions = [...new Set(getRecords.map(func => func.CS_Function.display_value))];
       const getObjectives = [...new Set(getRecords.map(rec => rec.Objective.display_value))];
       const getStandards = [...new Set(getRecords.map(rec => rec.Standard_Set.display_value))];
 
-      // Set state
+
       setObjectives(getObjectives);
       setAllFunctions(getFunctions);
       setGoals(getGoals);
       setAllRecords(getRecords);
-      setRecords(getRecords); // Set records to original data
+      setRecords(getRecords);
       setStandards(getStandards);
+      setLoading(false);
     };
     fetch();
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getRecords = filterRecords();
     setRecords(getRecords);
-  },[goal, functionValue, objective, standardSet])
+  }, [goal, functionValue, objective, standardSet])
   const filterRecords = () => {
     return allRecords.filter(record => {
       const matchesGoal = goal === "" || record.CR_Goal.display_value === goal;
@@ -85,54 +86,69 @@ const App = () => {
 
   return (
     <div className='p-2 bg-gray-100 min-h-[100vh]'>
-      <div className='bg-blue-600 text-white shadow-md rounded flex justify-between p-3'>
-        <div className='flex justify-around w-full'>
-          <Form.Group>
-            <FormLabel>Goals</FormLabel>
-            <Form.Select className='w-[200px]' onChange={handleGoalSelect} value={goal}>
-              <option value="">Select</option>
-              {goals.map((rec, index) => (
-                <option value={rec} key={index}>{rec}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <Form.Group>
-            <FormLabel>Functions</FormLabel>
-            <Form.Select className='w-[200px]' onChange={handleFunctionsChange} value={functionValue}>
-              <option value="">Select</option>
-              {allFunctions.map((rec, index) => (
-                <option value={rec} key={index}>{rec}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <Form.Group>
-            <FormLabel>Objectives</FormLabel>
-            <Form.Select className='w-[200px]' onChange={handleObjectiveChange} value={objective}>
-              <option value="">Select</option>
-              {objectives.map((rec, index) => (
-                <option value={rec} key={index}>{rec}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <Form.Group>
-            <FormLabel>Standard</FormLabel>
-            <Form.Select className='w-[200px]' onChange={handleStandardSetChange} value={standardSet}>
-              <option value="">Select</option>
-              {standards.map((rec, index) => (
-                <option value={rec} key={index}>{rec}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <div className='flex justify-center items-end'>
-            <Button variant='danger' size='sm' onClick={handleClear}>Clear</Button>
-          </div>
-        </div>
-      </div>
-      <div className='mt-3'>
-        {records.map((record, index) => (
-          <GuidelineComponent key={record.ID} index={index} record={record} />
-        ))}
-      </div>
+      {
+        loading === true ?
+          (
+            <>
+              <div className='text-center'>Loading...</div>
+            </>
+          )
+          :
+          (
+            <>
+              <div className='bg-blue-600 text-white shadow-md rounded flex justify-between p-3'>
+                <div className='flex justify-around w-full'>
+                  <Form.Group>
+                    <FormLabel>Goals</FormLabel>
+                    <Form.Select className='w-[200px]' onChange={handleGoalSelect} value={goal}>
+                      <option value="">Select</option>
+                      {goals.map((rec, index) => (
+                        <option value={rec} key={index}>{rec}</option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                  <Form.Group>
+                    <FormLabel>Functions</FormLabel>
+                    <Form.Select className='w-[200px]' onChange={handleFunctionsChange} value={functionValue}>
+                      <option value="">Select</option>
+                      {allFunctions.map((rec, index) => (
+                        <option value={rec} key={index}>{rec}</option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                  <Form.Group>
+                    <FormLabel>Objectives</FormLabel>
+                    <Form.Select className='w-[200px]' onChange={handleObjectiveChange} value={objective}>
+                      <option value="">Select</option>
+                      {objectives.map((rec, index) => (
+                        <option value={rec} key={index}>{rec}</option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                  <Form.Group>
+                    <FormLabel>Standard</FormLabel>
+                    <Form.Select className='w-[200px]' onChange={handleStandardSetChange} value={standardSet}>
+                      <option value="">Select</option>
+                      {standards.map((rec, index) => (
+                        <option value={rec} key={index}>{rec}</option>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                  <div className='flex justify-center items-end'>
+                    <Button variant='danger' size='sm' onClick={handleClear}>Clear</Button>
+                  </div>
+                </div>
+              </div>
+              <div className='mt-3'>
+                {records.map((record, index) => (
+                  <GuidelineComponent key={record.ID} index={index} record={record} />
+                ))}
+              </div>
+            </>
+          )
+      }
+
+
     </div>
   );
 };
